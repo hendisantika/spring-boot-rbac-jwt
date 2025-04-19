63b103c7700bca074541021795f0c639357f5f3e
hendisantika <hendisantika@yahoo.co.id>
2025-04-19 14:08:35 +0700

    feat: Add JwtAuthenticationFilter🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/rbacjwt/config/JwtAuthenticationFilter.java
@@ -0,0 +1,33 @@
+package id.my.hendisantika.rbacjwt.config;
+
+import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-rbac-jwt
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 19/04/25
+ * Time: 14.08
+ * To change this template use File | Settings | File Templates.
+ */
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Value("${jwt.header.string}")
+    public String HEADER_STRING;
+
+    @Value("${jwt.token.prefix}")
+    public String TOKEN_PREFIX;
+
+    @Resource(name = "userService")
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private TokenProvider jwtTokenUtil;
+}
