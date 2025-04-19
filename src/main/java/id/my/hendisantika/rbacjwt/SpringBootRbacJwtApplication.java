63006cb9cd83c1d7539e12120872f3f47036a4b8
hendisantika <hendisantika@yahoo.co.id>
2025-04-19 12:36:51 +0700

    feat: Initial Project🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/rbacjwt/SpringBootRbacJwtApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.rbacjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRbacJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRbacJwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/rbacjwt/SpringBootRbacJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.rbacjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRbacJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
