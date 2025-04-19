b6f672a9613bd0fafb29baf52abf9ea6c3c3c5e5
hendisantika <hendisantika@yahoo.co.id>
2025-04-19 12:42:56 +0700

    feat: Add LoginUser Model🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/rbacjwt/model/LoginUser.java
@@ -0,0 +1,22 @@
+package id.my.hendisantika.rbacjwt.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-rbac-jwt
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 19/04/25
+ * Time: 12.42
+ * To change this template use File | Settings | File Templates.
+ */
+@Getter
+@Setter
+public class LoginUser {
+    private String username;
+    private String password;
+}
