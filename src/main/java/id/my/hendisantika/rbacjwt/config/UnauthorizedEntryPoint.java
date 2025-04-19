3146d5cbf53b1d1b5d1df8491034ec35af5140c1
hendisantika <hendisantika@yahoo.co.id>
2025-04-19 14:15:03 +0700

    feat: Add UnauthorizedEntryPoint🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/rbacjwt/config/UnauthorizedEntryPoint.java
@@ -0,0 +1,30 @@
+package id.my.hendisantika.rbacjwt.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-rbac-jwt
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 19/04/25
+ * Time: 14.14
+ * To change this template use File | Settings | File Templates.
+ */
+@Component
+public class UnauthorizedEntryPoint implements AuthenticationEntryPoint, Serializable {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthenticated");
+    }
+}
