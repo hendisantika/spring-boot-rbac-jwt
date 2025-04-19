4ef79472effbc45158d8ffea9e4b7667081dba19
hendisantika <hendisantika@yahoo.co.id>
2025-04-19 14:10:11 +0700

    feat: Add TokenProvider🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/rbacjwt/config/TokenProvider.java
@@ -0,0 +1,31 @@
+package id.my.hendisantika.rbacjwt.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-rbac-jwt
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 19/04/25
+ * Time: 14.09
+ * To change this template use File | Settings | File Templates.
+ */
+@Component
+public class TokenProvider implements Serializable {
+
+    @Value("${jwt.token.validity}")
+    public long TOKEN_VALIDITY;
+
+    @Value("${jwt.signing.key}")
+    public String SIGNING_KEY;
+
+    @Value("${jwt.authorities.key}")
+    public String AUTHORITIES_KEY;
+
+}
