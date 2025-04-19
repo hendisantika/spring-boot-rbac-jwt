484aeefdca26954081091ee81c0a2393358c25e6
hendisantika <hendisantika@yahoo.co.id>
2025-04-19 12:41:18 +0700

    feat: Add AuthToken Model🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/rbacjwt/model/AuthToken.java
@@ -0,0 +1,25 @@
+package id.my.hendisantika.rbacjwt.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-rbac-jwt
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 19/04/25
+ * Time: 12.40
+ * To change this template use File | Settings | File Templates.
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthToken {
+    private String token;
+}
