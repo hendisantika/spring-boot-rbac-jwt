a221c1cb3100df08b49c75b9d12d345e6872fb3a
hendisantika <hendisantika@yahoo.co.id>
2025-04-19 14:15:47 +0700

    feat: Add WebSecurityConfig🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/rbacjwt/config/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package id.my.hendisantika.rbacjwt.config;
+
+import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-rbac-jwt
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 19/04/25
+ * Time: 14.15
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Resource(name = "userService")
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private PasswordEncoder encoder;
+
+    @Autowired
+    private UnauthorizedEntryPoint unauthorizedEntryPoint;
+
+}
